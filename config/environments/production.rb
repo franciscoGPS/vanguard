03bfe248855e25de12768242adbb7749a67c778b
Diana Enriquez <diana.enriquez@outlook.com>
2015-08-12 12:03:01 -0500

    Se agrego la configuracion inicial de Devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,8 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Devise config
+  config.action_mailer.default_url_options = { host: 'vanguardco.herokuapp.com'}
   # Config Mandrill
   config.action_mailer.smtp_settings = {
       :address              => "smtp.mandrillapp.com",
