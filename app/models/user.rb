d90364b36ada19b248edc555a090a43e5edabe01
Diana Enriquez <diana.enriquez@outlook.com>
2015-08-12 12:38:23 -0500

    Se agregó "lockable" y "remembable" configuracion a Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Include default devise modules.
   devise :database_authenticatable, :recoverable,
-         :rememberable, :trackable, :validatable
+         :rememberable, :trackable, :validatable,
+         :lockable
   acts_as_paranoid
 end
