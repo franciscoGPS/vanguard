ea745951823bbda2d6d80711393365284d3254fb
Francisco Cordero <fcordero@agaveti.com>
2015-08-17 19:21:46 -0500

    Se agrega el rol role, y su relación con admin y user.

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,5 +1,6 @@
 class Admin < ActiveRecord::Base
-	belong_to :role
+  
+  belongs_to :role
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, #:registerable, , :rememberable
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
-class User < ActiveRecord::
-	belongs_to :role
+class User < ActiveRecord::Base
+
   # Include default devise modules.
   devise :database_authenticatable, :recoverable,
          :rememberable, :trackable, :validatable,
          :lockable
+
   acts_as_paranoid
+
+  belongs_to :role
+  #before_create :set_default_role
+  #private
+  #def set_default_role
+  #  self.role ||= Role.find_by_name('registered')
+  #end
 end
