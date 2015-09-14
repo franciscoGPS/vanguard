462d82195d4c4fe41616e74ec95d6adca87b4b84
Diana Enriquez <diana.enriquez@outlook.com>
2015-09-14 09:43:05 -0500

    Merge 'dev' branch into preembarques

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,9 @@
 Rails.application.routes.draw do
 
-<<<<<<< HEAD
-  root "static_pages#index"
-
-  devise_for :users
-  resources :users
-  resources :roles
-  resources :greenhouses
-  resources :contacts
-  resources :products
-  resources :shipments
-
-  get 'preshipments' => 'greenhouses#shipments'
-=======
-  
   root "static_pages#index"
   get 'admin', to: "static_pages#admin"
 
-  scope :admin  do 
+  scope :admin  do
     resources :customers
     resources :box_types
     resources :bag_types
@@ -30,6 +16,7 @@ Rails.application.routes.draw do
     resources :contacts
     resources :products
     resources :customers
+    get 'preshipments' => 'greenhouses#shipments'
   end
 
   # Website pages
@@ -38,5 +25,4 @@ Rails.application.routes.draw do
   get 'contact', to: "static_pages#contact", as: "web_contact"
   get 'solutions', to: "static_pages#solutions"
 
->>>>>>> dev
 end
