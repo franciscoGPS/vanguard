25bd09341efaf05fa1e72c36063051ef119ccd01
Diana Enriquez <diana.enriquez@outlook.com>
2015-10-08 22:02:37 -0500

    HOTFIX Iconos de las acciones se pintan de colores diferentes.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,10 @@
 module ApplicationHelper
 
-def bootstrap_class_for flash_type
-    case flash_type
-      when :success
-        "alert-success"
-      when :error
-        "alert-danger"
-      when :alert
-        "alert-block"
-      when :notice
-        "alert-info"
-      else
-        flash_type.to_s
-    end
+  def bootstrap_flash_class(flash_type)
+      { success: 'alert-success',
+        error:   'alert-danger',
+        alert:   'alert-warning',
+        notice:  'alert-info'
+      }[flash_type.to_sym] || flash_type.to_s
   end
-
 end
