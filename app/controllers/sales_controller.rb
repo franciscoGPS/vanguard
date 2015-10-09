7fdc0bb55552364f5a9c25046de2b99effed2353
Francisco Cordero <fcordero@agaveti.com>
2015-10-09 17:53:52 -0500

    Funciona la creación de ventas, edición de precios en la venta.

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -5,6 +5,7 @@ class SalesController < ApplicationController
   # GET /sales.json
   def index
     @sales = Sale.all
+
   end
 
   # GET /sales/1
@@ -24,8 +25,8 @@ class SalesController < ApplicationController
 
       @sale = Sale.new
       @shipments = Shipment.unconfirmed
-     else
-        respond_to do |format|
+    else
+      respond_to do |format|
         format.html { redirect_to Shipment.new, notice: 'No unconfirmed shipments.' }
       end
     end
@@ -34,61 +35,89 @@ class SalesController < ApplicationController
 
   # GET /sales/1/edit
   def edit
+    @shipments = Shipment.where(sale_id: params[:id])
+    @action = params[:action]
+
   end
 
   # POST /sales
   # POST /sales.json
   def create
     @sale = Sale.new(sale_params)
-
-    @shipments_selected = params[:sale].has_key?(:shipment_ids) ? Shipment.find(params[:shipment_ids]) : Array.new
-
-
-
-      if @shipments_selected.present?
-
-
-
-        if @sale.save
-            @shipments_selected.each do |shipment|
-              shipment.sale_id = @sale.id
-
-              price = params[shipment.id.to_s]
-
-              # Aquí price tiene un arreglo de valores. Solo obtenemos su único valor.
-              shipment.price = price[0]
-              if shipment.save
-                 respond_to do |format|
-                 format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
-               end
-              end
-           end
-          #format.json { render :show, status: :created, location: @sale }
-          #else
-          #format.json { render json: @sale.errors, status: :unprocessable_entity }
-        end # if @sale.save
-
-      else #if !(@shipments_selected.empty?)
-          @shipments = Shipment.unconfirmed
+    @shipments_selected = shipments_selected
+    byebug
+    if @shipments_selected.present?
+      @sale.user_id = current_user.id
+      if @sale.save
+        @shipments_selected.each do |shipment|
+          shipment.sale_id = @sale.id
+          price = params[shipment.id.to_s]
+          # Aquí price tiene un arreglo de valores. Solo obtenemos su único valor.
+          shipment.price = price
+          begin
+            shipment.save
             respond_to do |format|
-              format.html { redirect_to new_sale_path, error: 'No Shipments added to the Sale. Please verify.' }
-              byebug
+              format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
             end
-      end # if !(@shipments_selected.empty?)
+          rescue Exception => e
+            respond_to do |format|
+              format.html { redirect_to @sale, error: 'No shipments persisted' }
+            end
+          end
+        end
+      else
+        respond_to do |format|
+          format.html { redirect_to @sale, error: 'No shipments persisted' }
+        end
+
+        #format.json { render :show, status: :created, location: @sale }
+        #else
+        #format.json { render json: @sale.errors, status: :unprocessable_entity }
+      end # if @sale.save
+
+    else #if !(@shipments_selected.empty?)
+      @shipments = Shipment.unconfirmed
+      respond_to do |format|
+        format.html { redirect_to new_sale_path, error: 'No Shipments added to the Sale. Please verify.' }
+        byebug
+      end
+    end # if !(@shipments_selected.empty?)
+  end
 
 
-
+  def shipments_selected
+    return shipments_selected = params.has_key?(:shipment_ids) ? Shipment.find(params[:shipment_ids]) : Array.new
   end
 
   # PATCH/PUT /sales/1
   # PATCH/PUT /sales/1.json
   def update
-    respond_to do |format|
-      if @sale.update(sale_params)
-        format.html { redirect_to @sale, notice: 'Sale was successfully updated.' }
+    if @sale.update(sale_params)
+      @shipments_to_update = Shipment.to_edit(@sale.id)
+      #@shipments_to_update = Shipment.find_by_sale_id(@sale_id)
+      if @shipments_to_update.present?
+        @shipments_to_update.each do |shipment|
+          test_prices = params[shipment.id.to_s]
+          shipment.sale_id = @sale.id
+          price = params[shipment.id.to_s]
+          shipment.price = price
+          begin
+            shipment.save
+            respond_to do |format|
+              format.html { redirect_to @sale, notice: 'Sale was successfully created.' }
+            end
+          rescue Exception => e
+            flash[:error] = 'No shipments persisted'
+          end
+
+        end
+
+        #format.html { redirect_to @sale, notice: 'Sale was successfully updated.' }
         #format.json { render :show, status: :ok, location: @sale }
       else
-        format.html { render :edit }
+        respond_to do |format|
+          format.html { render :edit }
+        end
         #format.json { render json: @sale.errors, status: :unprocessable_entity }
       end
     end
@@ -105,15 +134,21 @@ class SalesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_sale
-      @sale = Sale.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_sale
+    @sale = Sale.find(params[:id])
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def sale_params
-      accessible = [:season, :departure_date, :arrival_date, :manifest, :annotation, :comment, :password_confirmation, :current_password]
-      #accessible << [role_attributes: [:id, :name]]
-      params.require(:sale).permit(accessible)
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def sale_params
+    accessible = [:season, :departure_date, :arrival_date, :manifest, :annotation,
+    :comment, :user_id]
+    #accessible << [role_attributes: [:id, :name]]
+    params.require(:sale).permit(accessible)
+  end
 end
+
+
+
+
+
--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,5 +1,6 @@
 class Customer < ActiveRecord::Base
   has_many :contacts, :as => :contactable, :class_name => "Contact", dependent: :destroy
+  has_many :sales
   accepts_nested_attributes_for :contacts, :reject_if => :all_blank, :allow_destroy => true
 
   acts_as_paranoid
