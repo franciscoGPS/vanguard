e71e1d1b5b8a4e15f0bec98d658b320505eb61f9
Francisco Cordero <fcordero@agaveti.com>
2015-11-28 20:54:59 -0600

    HOTFIX Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,8 @@
 # 	Pallet_type.create([{name: 'chico'},{name: 'mediano'},{name: 'grande'}])
 #   Bag_type.create([{name: 'chico'},{name: 'mediano'},{name: 'grande'}])
 Role.create!( name: "Admin")
-User.create!( name: "Admin", job: "Systems", password: '.Agave15!', email: 'hola@agaveti.com', admin: true, role_id: 1)
-User.create!( name: "Admin", job: "Vanguard Admin", password: 'Prueba123!', email: 'diana.s@vanguardco.com.mx', admin: true, role_id: 1)
+User.new (name: "Admin", job: "Systems", password: '.Agave15!', email: 'hola@agaveti.com', admin: true, role_id: 1).save
+User.new( name: "Admin", job: "Vanguard Admin", password: 'Prueba123!', email: 'diana.s@vanguardco.com.mx', admin: true, role_id: 1).save
 
 
 # Sets superadmin attribute
