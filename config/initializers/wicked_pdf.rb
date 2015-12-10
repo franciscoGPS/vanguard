e8cda4acfb03bf16e4689a94b0e264151409a9b2
Diana Enriquez <diana.enriquez@outlook.com>
2015-12-10 17:45:53 -0600

    HOTFIX Se descomprimió la libreria para wicked_pdf

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,5 +1,5 @@
-WickedPdf.config = {
-  #:wkhtmltopdf => '/usr/local/bin/wkhtmltopdf',
-  #:layout => "pdf.html",
-  #:exe_path => '/usr/local/bin/wkhtmltopdf'
-}
+if Rails.env.staging? || Rails.env.production?
+  WickedPdf.config = {:exe_path => Rails.root.join('bin', 'wkhtmltopdf-amd64').to_s}
+else
+  WickedPdf.config = { :exe_path => '/usr/bin/wkhtmltopdf'}
+end
