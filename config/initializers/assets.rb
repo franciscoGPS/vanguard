e14e7e4b75d581ad19077da6a53a2b96f36a4464
Francisco Cordero <fcordero@agaveti.com>
2016-01-14 13:16:27 -0600

    WIKED PDF Works local. REPLACED pdf documents.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -12,3 +12,4 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( home.css )
 Rails.application.config.assets.precompile += %w( fonts.css )
 Rails.application.config.assets.precompile += %w( main.css )
+Rails.application.config.assets.precompile += ['bootstrap.css.scss']
