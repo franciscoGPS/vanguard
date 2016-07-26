bb1f1182cb1a3663c0e5d93606a0ddc0dbaa1b4c
Francisco Cordero <fcordero@agaveti.com>
2016-07-26 09:09:53 -0600

    FIXED update sale - warehouse_id flow

--- app/controllers/sales_controller.rb
+++ app/controllers/sales_controller.rb
@@ -127,13 +127,17 @@ class SalesController < ApplicationController
   def update
     @greenhouse = Greenhouse.find(params[:greenhouse_id])
     begin
+      byebug
       modified_sale = Sale.find(params[:id].to_i)
         if(modified_sale.warehouse_id != params[:sale][:warehouse_id])
           manifest = Manifest.where(:sale => params[:id].to_i).first
-          manifest.warehouse_id = params[:sale][:warehouse_id]
-          manifest.save
+          if manifest != nil
+            manifest.warehouse_id = params[:sale][:warehouse_id]
+            manifest.save
+          end
+          @sale.update(sale_params)
         end
-        @sale.update(sale_params)
+
     rescue Exception => e
         if e.cause.class.to_s == "PG::UniqueViolation"
          flash[:error] = "Attempted to use an already used Shipment Consecutive.
