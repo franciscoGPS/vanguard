c4a2819b88ca1123f2e8e496c833089ab80ad849
Francisco Cordero <franciscoGPS@users.noreply.github.com>
2019-01-28 22:46:58 -0600

    Updated features

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,19 +3,6 @@
 require 'ffaker'
 Role.create( name: "Admin")
 
-User.create(name: "Admin",
-            job: "Systems",
-            email: "hola@agaveti.com",
-            password: ".Agave15!",
-            admin: true,
-            role_id: 1, #Admin
-            superadmin: true)
-User.create(name: "Diana Saenz",
-            job: "Admin Vanguard",
-            email: "diana.s@vanguardco.com.mx",
-            password: "Prueba123!",
-            admin: true,
-            role_id: 1)
 
 Greenhouse.public_activity_off
 Product.public_activity_off
